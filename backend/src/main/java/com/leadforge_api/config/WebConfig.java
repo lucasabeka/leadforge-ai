5987130cfe36e5102901e4787ebf3ecf587ad9d3
lucasabeka <lucasabekadoth@gmail.com>
2026-02-11 16:08:51 +0100

    fix : configuration SQL pour le déploiement

--- backend/src/main/java/com/leadforge_api/config/WebConfig.java
+++ backend/src/main/java/com/leadforge_api/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${cors.allowed.origins:http://localhost:4200}")
+    @Value("${cors.allowed.origins}")
     private String allowedOrigins;
 
     @Override
