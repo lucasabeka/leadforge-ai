5880a3061e886fc830c482a70b1e671159aa99f0
lucasabeka <lucasabekadoth@gmail.com>
2026-02-16 08:44:21 +0100

    fix: Add standalone flag to all components

--- frontend/src/app/features/campaign/list/list.component.ts
+++ frontend/src/app/features/campaign/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list',
+  standalone: true,
   imports: [],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
