eed843bf9c00ec547afc8878895e54d78814a05a
lucasabeka <lucasabekadoth@gmail.com>
2026-02-16 09:42:02 +0100

    fix: Remove @CrossOrigin annotations from controllers (use global CORS config)

--- backend/src/main/java/com/leadforge_api/controller/CampaignController.java
+++ backend/src/main/java/com/leadforge_api/controller/CampaignController.java
@@ -18,7 +18,6 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/campaigns")
-@CrossOrigin(origins = "http://localhost:4200")
 public class CampaignController {
 
     @Autowired
--- backend/src/main/java/com/leadforge_api/controller/CreditController.java
+++ backend/src/main/java/com/leadforge_api/controller/CreditController.java
@@ -15,7 +15,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/credits")
-@CrossOrigin(origins = "*")
 public class CreditController {
 
     @Autowired
